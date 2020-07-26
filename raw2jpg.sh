97da2c1ff99f81fd68e73fff06f426661f6d4326
ZOulhadj <zakariyaoulhadj01@gmail.com>
2020-07-26 20:01:09 +0100

    Made some changes

--- raw2jpg.sh
+++ raw2jpg.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+# Note: SIPS requires 'jpeg' instead of jpg for such files
+
 old_format=CR2
-new_format=jpg
-quality=80
+new_format=jpeg
+compression_quality=80
 
-output=output
-
-mkdir -p ./$output
+output_folder=output
 
 file_count=0
 
-for i in *.$old_format
-do
-    sips -s format jpeg -s formatOptions $quality $i --out "./$output/${i%.*}.$new_format"
+# TODO: Create a boolean flag to only create a folder if at least one file has been found
+mkdir -p ./$output_folder
+
+for i in *.$old_format; do
+    sips -s format $new_format -s formatOptions $compression_quality $i --out "./$output_folder/${i%.*}.$new_format"
     file_count=$((file_count+1))
 done
 
-echo "Exported $file_count image(s) from $old_format to $new_format into the $output directory"
+echo "Exported $file_count image(s) from $old_format to $new_format into the $output_folder directory"
